769b777c8f498405cbfd4c29d70d28b51e7f5f9e
dumpdusty <dumpdusty@gmail.com>
2023-02-02 10:36:27 -0800

    upd globval hook, helpers and installation

--- project-config/auth-global-hook.js
+++ project-config/auth-global-hook.js
@@ -3,5 +3,5 @@ import { login } from '../helpers/general-helper.js'
 
 before(async () => {
     const response = await login(process.env.EMAIL, process.env.PASSWORD)
-    process.env['TOKEN'] = response.body.payload.token
+    process.env.TOKEN = response.body.payload.token
 })
