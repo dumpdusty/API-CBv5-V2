f8502454b46e07436b4901cfef56e06d578a0aca
dumpdusty <dumpdusty@gmail.com>
2023-03-01 11:24:07 -0800

    update service spec-2

--- tests/service.spec.js
+++ tests/service.spec.js
@@ -8,13 +8,13 @@ describe('Service Tests', () => {
         let res
         let vendorId
         before(async ()=>{
-            vendorId = await vendorHelper.createVendor()
+            vendorId = (await vendorHelper.createVendor()).body.payload
             res = await request(process.env.BASE_URL)
                 .post('/v5/service')
                 .set('Authorization', process.env.TOKEN)
-                .send({name:`service_${Date.now()}`, vendor:`63fe9c1ac1c60ba19649b252`, vendorPrice: 222, clientPrice: 444})
+                .send({name:`service_${Date.now()}`, vendor:`${vendorId}`, vendorPrice: 222, clientPrice: 444})
 
-            console.log(vendorId.body.payload)
+            console.log(vendorId)
         })
 
         it('check the status code', () => {
