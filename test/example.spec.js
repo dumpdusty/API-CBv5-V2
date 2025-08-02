33027fcceccde18f117cd264af29f1f53156e487
Fazil M <dumpdusty@gmail.com>
2025-08-01 19:41:52 -0700

    add multiply test

--- test/example.spec.js
+++ test/example.spec.js
@@ -4,4 +4,8 @@ describe('The first test', () => {
     it('Check if sum working properly', () => {
         expect(5+5).to.eq(10)
     });
+
+    it('Check if sum working properly', () => {
+        expect(5*5).to.eq(25)
+    });
 });
